c42595d77239e7b87363049c912cf39ec37a18d9
weaponsforge <forgeweapons@gmail.com>
2023-02-25 16:27:35 +0800

    feat: Initial integration of mui5 @v5.11.0 nextjs example files

---
+++ client/src/lib/mui/theme.js
@@ -0,0 +1,32 @@
+import { Roboto } from '@next/font/google'
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+// Source: https://github.com/mui/material-ui/tree/master/examples/material-next
+// Date: 20230225 @v5.11.10
+export const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  fallback: ['Helvetica', 'Arial', 'sans-serif'],
+})
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+})
+
+export default theme
