ecc78f1363c941bf4c1e3978479543b64e80791a
vikyw89 <vikyw89@gmail.com>
2023-02-27 01:17:24 +0800

    chore: updated Login page UI

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -1,20 +1,72 @@
 import PropTypes from 'prop-types'
-
+// import Image from "next/image";
 import Typography from '@mui/material/Typography'
 import Page from '@/common/layout/page'
-
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import Paper from '@mui/material/Paper'
 function LoginComponent ({ simcards, stringNames }) {
   return (
     <Page>
-      <Typography variant="h2" component="h2">
-        Login
-      </Typography>
-
-      {simcards.map((item, index) => (
-        <p key={index}>{item.name}</p>
-      ))}
-
-      <p>{stringNames}</p>
+      <Box sx={{
+        outline: '3px solid green',
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '20px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundImage: 'url(/pexels-pixabay-101529.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundOrigin: 'border-box',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        position: 'relative',
+        width: '100%',
+        height: '80vh',
+        overflow: 'hidden',
+        boxShadow: 'rgba(0, 0, 0, 0.5) 0px 5px 10px 0px inset',
+        borderRadius: '20px',
+        marginBottom: '-50px',
+      }}>
+        <Paper elevation={20} sx={{
+          margin: '20px', 
+          flex: 10,
+          borderRadius: '30px',
+          position: 'relative',
+          background: 'inherit',
+          minWidth: '300px',
+          maxWidth: '500px',
+          border: '1px solid green'
+        }}>
+          <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            borderRadius: '30px',
+            gap: '13px',
+            padding: '40px',
+            position: 'relative',
+            backdropFilter: 'blur(3px)',
+            zIndex: 9,
+            minWidth: '300px',
+            maxWidth: '500px'
+          }}>
+            <Typography variant="h5" sx={{ fontWeight: 'bold', textDecoration: 'underline'}}>
+              Login :
+            </Typography>
+            <TextField
+              label="email"
+              id="email"
+              size="small"
+            />
+            <TextField
+              label="password"
+              id="password"
+              size="small"
+            />
+          </Box>
+        </Paper>
+      </Box>
     </Page>
   )
 }
