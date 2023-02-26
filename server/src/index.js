bd3c848cdd2f869f0bcc4f03038dac4cab710406
weaponsforge <forgeweapons@gmail.com>
2023-02-26 10:40:09 +0800

    chore: Add prod/dev log on server index

--- server/src/index.js
+++ server/src/index.js
@@ -8,13 +8,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static('public'))
 
 app.get('*', (req, res) => {
-  return res.status(200).send('Welcome to the My Phonebook server')
+  return res.status(200).send(`Welcome to My Phonebook server\nRunning in [${process.env.NODE_ENV}] mode`)
 })
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
-  console.log(`My Phonebook server\nlistening on http://localhost:${PORT}`)
+  console.log(`My Phonebook server [${process.env.NODE_ENV}]\nlistening on http://localhost:${PORT} mode`)
 })
