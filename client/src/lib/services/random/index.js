10421721f90489305f7e651ad6ad6aa7705cc623
vikyw89 <vikyw89@gmail.com>
2023-03-01 02:03:03 +0800

    fix: lint

--- client/src/lib/services/random/index.js
+++ client/src/lib/services/random/index.js
@@ -1,10 +1,9 @@
 export const getRandomJoke = async() => {
-    try {
-        const API = process.env.NEXT_PUBLIC_RANDOM_JOKE_API
-        const response = await fetch(API)
-        const randomJoke = await response.json()
-        return randomJoke
-    } catch(err) {
-        console.log(err)
-    }
+  try {
+    const API = process.env.NEXT_PUBLIC_RANDOM_JOKE_API
+    const response = await fetch(API)
+    const randomJoke = await response.json()
+    return randomJoke
+  } catch(err) {
+  }
 }
