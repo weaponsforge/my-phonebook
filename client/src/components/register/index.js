0e66d6153160e47323699d96645d82a8cb02ad94
vikyw89 <vikyw89@gmail.com>
2023-03-01 22:28:38 +0800

    chore: progress on registerComponent

---
+++ client/src/components/register/index.js
@@ -0,0 +1,124 @@
+import Page from "@/common/layout/page"
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
+import Typography from '@mui/material/Typography'
+import { getRandomJoke } from '@/lib/services/random'
+import { useEffect, useState } from 'react'
+const RegisterComponent = () => {
+    const [joke, setJoke] = useState()
+    const [username, setUsername] = useState({ error:false, helperText:' ',value:'', color:'text' })
+    const [password, setPassword] = useState({ error:false, helperText:' ',value:'', color:'text' })
+    const [passwordConfirmation, setPasswordConfirmation] = useState({ error:false, helperText:' ',value:'', color:'text' })
+    useEffect(()=>{
+        setTimeout(async()=>{
+          const randomJoke = await getRandomJoke()
+          setJoke(randomJoke)
+        })
+      },[])
+
+    const usernameHandler = (e) => {
+    const {helperText, color} = Validate.email(e.target.value)
+    const newUsername = {
+        ...username,
+        value: e.target.value,
+        error: false,
+        helperText,
+        color
+    }
+    setUsername(newUsername)
+    }
+
+    const passwordHandler = (e) => {
+    const {helperText, color } = Validate.password(e.target.value)
+    const newPassword = {
+        ...password,
+        value: e.target.value,
+        error: false,
+        helperText,
+        color
+    }
+    setPassword(newPassword)
+    }
+    return (
+        <Page>
+            <Paper sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: '10px',
+                alignItems: 'center',
+                justifyContent: 'center',
+                flex:1,
+                background: 'inherit',
+                flexWrap: 'wrap-reverse'
+            }}>
+                <Typography variant="h8" component="h3" gutterBottom sx={{ color:(theme)=>theme.palette.text.disabled, textAlign: 'center', paddingLeft: '20px',paddingRight:'20px', maxWidth: '50vw' }}>
+                    `{joke && joke.joke}`
+                </Typography>
+                <Paper elevation={0} sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px',
+                    minWidth: '300px',
+                    width: '500px',
+                    borderRadius: '30px',
+                    padding: '40px',
+                    background: 'inherit',
+                }}>
+                    {/* registration form goes here */}
+                    <TextField
+                        label="Username"
+                        id="username"
+                        size="small"
+                        type="text"
+                        fullwidth="true"
+                        required={true}
+                        color={username.color}
+                        helperText={username.helperText}
+                        value={username.value}
+                        error={username.error}
+                        onChange={usernameHandler}
+                    />
+                    <TextField
+                        label="Password"
+                        id="userPassword"
+                        size="small"
+                        type="password"
+                        fullwidth="true"
+                        required={true}
+                        value={password.value}
+                        error={password.error}
+                        color={password.color}
+                        helperText={password.helperText}
+                        onChange={passwordHandler}
+                    />
+                    <TextField
+                        label="Password"
+                        id="userPassword"
+                        size="small"
+                        type="password"
+                        fullwidth="true"
+                        required={true}
+                        value={passwordConfirmation.value}
+                        error={passwordConfirmation.error}
+                        color={passwordConfirmation.color}
+                        helperText={passwordConfirmation.helperText}
+                        onChange={passwordConfirmationHandler}
+                    />
+                    <Button 
+                        variant="contained" 
+                        sx={{
+                        fontWeight:'bold',
+                        color: (theme)=>theme.palette.primary.contrastText
+                        }}
+                    >
+                        REGISTER
+                    </Button>
+                </Paper>
+            </Paper>
+        </Page>
+    )
+}
+
+export { RegisterComponent }
---
+++ client/src/pages/register/index.js
@@ -0,0 +1,10 @@
+import { RegisterComponent } from "@/components/register"
+
+
+const Register = () => {
+    return (
+        <RegisterComponent/>
+    )
+}
+
+export default Register
