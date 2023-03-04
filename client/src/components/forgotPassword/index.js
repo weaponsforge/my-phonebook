cbeb0cf4a12718806e1c71c6e2ec7d10d163f207
vikyw89 <vikyw89@gmail.com>
2023-03-04 10:48:34 +0800

    chore: forgotPasswordPage

--- client/src/components/forgotPassword/index.js
+++ client/src/components/forgotPassword/index.js
@@ -1,11 +1,99 @@
-import Page from "@/common/layout/page"
+import Page from '@/common/layout/page'
+import { getRandomJoke } from '@/lib/services/random'
+import { Validate } from '@/lib/utils/textValidation'
+import { Button, Paper, TextField, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
+import CheckIcon from '@mui/icons-material/Check'
 
 const ForgotPasswordComponent = () => {
-    return (
-        <Page>
-            Test
-        </Page>
-    )
+  const [username, setUsername] = useState({ error:true, helperText:' ',value:'', color:'text' })
+  const [joke, setJoke] = useState()
+  useEffect(()=>{
+    setTimeout(async()=>{
+      const randomJoke = await getRandomJoke()
+      setJoke(randomJoke)
+    })
+  },[])
+  const usernameHandler = (e) => {
+    const {helperText, error, color} = Validate.email(e.target.value)
+    const newUsername = {
+      ...username,
+      value: e.target.value,
+      error,
+      helperText,
+      color
+    }
+    setUsername(newUsername)
+  }
+  const resetPasswordHandler = () => {
+    // to integrate with API route
+  }
+  return (
+    <Page>
+      <Paper sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '10px',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex:1,
+        background: 'inherit',
+        flexWrap: 'wrap-reverse'
+      }}>
+        <Typography variant="h8" component="h3" gutterBottom sx={{
+          color:(theme)=>theme.palette.text.disabled, 
+          textAlign: 'center', 
+          paddingLeft: '20px',
+          paddingRight:'20px', 
+          width: '50vw' }}>
+            `{joke && joke.joke}`
+        </Typography>
+        <Paper elevation={0} sx={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 20px',
+          gridTemplateAreas:
+              `"username icon1"
+              "resetPassword ."`,
+          alignItems:'stretch',
+          gap: '10px',
+          minWidth: '300px',
+          width: '500px',
+          borderRadius: '30px',
+          padding: '40px',
+          background: 'inherit',
+        }}>
+          <TextField
+            label="Username (email)"
+            id="username"
+            size="small"
+            type="text"
+            fullwidth="true"
+            required={true}
+            color={username.color}
+            helperText={username.helperText}
+            value={username.value}
+            error={username.error}
+            onChange={usernameHandler}
+            sx={{ gridArea:'username'}}
+          />
+          {!username.error &&
+              <CheckIcon fontSize="large" color="success" sx={{ gridArea:'icon1' }}/>
+          }
+          <Button 
+            variant="contained" 
+            sx={{
+              fontWeight:'bold',
+              color: (theme)=>theme.palette.primary.contrastText,
+              gridArea: 'resetPassword',
+            }}
+            onClick={resetPasswordHandler}
+          >
+              RESET PASSWORD
+          </Button>
+        </Paper>
+      </Paper>
+    </Page>
+  )
 }
 
 export default ForgotPasswordComponent
