d6ee6d0e6650473817aeef6f629dc3b2a1b662b5
vikyw89 <vikyw89@gmail.com>
2023-03-13 13:34:14 +0800

    fix: lint

--- client/src/domain/account/resetpassword.js
+++ client/src/domain/account/resetpassword.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 
-import { useGlobalState } from '@/lib/hooks/useGlobalState'
 import { Validate } from '@/lib/utils/textValidation'
 
 import Button from '@mui/material/Button'
