1720f74a5973af28c7bfa1363b2828cd79a4c332
vikyw89 <vikyw89@gmail.com>
2023-03-17 17:00:32 +0800

    chore: controlled field on viewContact

--- client/src/components/contactList/contactListSidebar/viewContact.js/index.js
+++ client/src/components/contactList/contactListSidebar/viewContact.js/index.js
@@ -1,15 +1,26 @@
 import { useSyncGlobalVariable } from "@/lib/hooks/useSync"
-import { Avatar, Box, TextField, Typography } from "@mui/material"
-import { useRef } from "react"
+import { Avatar, Box, Button, TextField, Typography } from "@mui/material"
+import { useRef, useState } from "react"
 
 export const ViewContact = () => {
     const [viewContact, setViewContact] = useSyncGlobalVariable('viewContact')
+    const [state, setState] = useState({isUpdated:false})
+
     const focusElement = useRef()
     if (!viewContact) {
         return
     }
-    
-    focusElement?.current?.scrollIntoView({behaviour:'smooth'})
+
+    focusElement?.current?.scrollIntoView({ behaviour: 'smooth' })
+
+    const editContactHandler = (e) => {
+        const fieldID = e.target.id
+        const fieldValue = e.target.value
+        setViewContact({
+            ...viewContact,
+            [fieldID]:fieldValue
+        })
+    }
     return (
         <Box ref={focusElement}
             sx={{
@@ -45,28 +56,56 @@ export const ViewContact = () => {
                     alignItems: 'center',
                     gap: '10px'
                 }}>
-                    <Typography variant="h8" sx={{}}>
+                    <Typography variant="h8">
                         First Name :
                     </Typography>
-                    <TextField value={viewContact.first_name} size="small" sx={{ width: '100%' }} />
-
+                    <TextField id="first_name" value={viewContact.first_name} size="small" sx={{ width: '100%' }} onChange={editContactHandler}/>
                 </Box>
-                <Typography variant="h8">
-                    Middle Name :
-                </Typography>
-                <TextField value={viewContact.middle_name} size="small" />
-                <Typography variant="h8">
-                    Last Name:
-                </Typography>
-                <TextField value={viewContact.last_name} size="small" />
-                <Typography variant="h8">
-                    Contact Number:
-                </Typography>
-                <TextField value={viewContact.contact_no} size="small" />
-                <Typography variant="h8">
-                    Contact Email:
-                </Typography>
-                <TextField value={viewContact.contact_email} size="small" />
+                <Box sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    gap: '10px'
+                }}>
+                    <Typography variant="h8">
+                        Middle Name :
+                    </Typography>
+                    <TextField id="middle_name" value={viewContact.middle_name} size="small" sx={{ width: '100%' }} onChange={editContactHandler}/>
+                </Box>
+                <Box sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    gap: '10px'
+                }}>
+                    <Typography variant="h8">
+                        Last Name:
+                    </Typography>
+                    <TextField id="last_name" value={viewContact.last_name} size="small" sx={{ width: '100%' }} onChange={editContactHandler}/>
+                </Box>
+                <Box sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    gap: '10px'
+                }}>
+                    <Typography variant="h8">
+                        Contact Number:
+                    </Typography>
+                    <TextField id="contact_no" value={viewContact.contact_no} size="small" sx={{ width: '100%' }} onChange={editContactHandler}/>
+                </Box>
+                <Box sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    alignItems: 'center',
+                    gap: '10px'
+                }}>
+                    <Typography variant="h8">
+                        Contact Email:
+                    </Typography>
+                    <TextField id="contact_email" value={viewContact.contact_email} size="small" sx={{ width: '100%' }} onChange={editContactHandler}/>
+                </Box>
+                <Button variant="contained" fullWidth disabled={!state.isUpdated}>Save</Button>
             </Box>
         </Box>
     )
