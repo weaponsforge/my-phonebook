74bf9d26d76d420755c7bc1ace1cc14590e4db3f
vikyw <vikyw89@gmail.com>
2023-03-28 12:36:52 +0800

    update: hide contact display when typing on search

--- client/src/components/contacts/sidebar/searchField/searchField.js
+++ client/src/components/contacts/sidebar/searchField/searchField.js
@@ -1,15 +1,24 @@
 import { Box, TextField } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
-import { createSyncV, useSyncV } from 'use-sync-v'
+import { createSyncV, updateSyncV, useSyncV } from 'use-sync-v'
 
 export const SearchField = () => {
   const { searchKeyword } = useSyncV('ui.search')
   const searchFieldHandler = (e) => {
     e.stopPropagation()
     createSyncV('ui.search.searchKeyword', e.target.value)
+    updateSyncV('ui.phase', (p) => ({
+      ...p,
+      createContact: false,
+      editContact: false,
+    }))
     if (e.target.value === '') {
-      createSyncV('ui.phase.editContact', false)
-      createSyncV('ui.phase.search', false)
+      updateSyncV('ui.phase', (p) => ({
+        ...p,
+        createContact: false,
+        editContact: false,
+        search: false,
+      }))
     } else {
       createSyncV('ui.phase.search', true)
     }
