6395ac9ca9866be15d64ba3899734ea84b772fa8
vikyw <vikyw89@gmail.com>
2023-04-05 14:33:31 +0800

    fix: animate button toogle icon

--- client/src/common/layout/header/index.js
+++ client/src/common/layout/header/index.js
@@ -1,90 +1,91 @@
 // REACT
-import { useState } from 'react'
+import { useState } from "react";
 
 // NEXT
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 // MUI
-import DarkModeIcon from '@mui/icons-material/DarkMode'
-import HowToRegIcon from '@mui/icons-material/HowToReg'
-import LightModeIcon from '@mui/icons-material/LightMode'
-import LoginIcon from '@mui/icons-material/Login'
-import MenuIcon from '@mui/icons-material/Menu'
-import AppBar from '@mui/material/AppBar'
-import Avatar from '@mui/material/Avatar'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Container from '@mui/material/Container'
-import IconButton from '@mui/material/IconButton'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import Toolbar from '@mui/material/Toolbar'
-import Tooltip from '@mui/material/Tooltip'
-import Typography from '@mui/material/Typography'
-import PlayArrowIcon from '@mui/icons-material/PlayArrow'
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import LoginIcon from "@mui/icons-material/Login";
+import MenuIcon from "@mui/icons-material/Menu";
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 
 // LIB
-import { authSignOut } from '@/lib/hooks/useInitAuth'
-import { setSyncLocalStorage, useSyncLocalStorage } from '@/lib/hooks/useSync'
-import { Avalon } from '@/lib/mui/theme'
-import { useSyncV } from 'use-sync-v'
+import { authSignOut } from "@/lib/hooks/useInitAuth";
+import { setSyncLocalStorage, useSyncLocalStorage } from "@/lib/hooks/useSync";
+import { Avalon } from "@/lib/mui/theme";
+import { useSyncV } from "use-sync-v";
 
 // VARIABLES
-const pages = ['about']
+const pages = ["about"];
 const settings = [
   {
-    name: 'Profile',
-    route: 'profile',
+    name: "Profile",
+    route: "profile",
   },
   {
-    name: 'Account',
-    route: '/',
+    name: "Account",
+    route: "/",
   },
   {
-    name: 'Contacts',
-    route: 'contacts',
+    name: "Contacts",
+    route: "contacts",
   },
   {
-    name: 'Logout',
-    route: '#',
+    name: "Logout",
+    route: "#",
   },
-]
+];
 
 function Header() {
   // HOOKS
-  const [anchorElNav, setAnchorElNav] = useState(null)
-  const [anchorElUser, setAnchorElUser] = useState(null)
-  const activeTheme = useSyncLocalStorage('activeTheme')
-  const animate = useSyncLocalStorage('animate')
-  const { authUser } = useSyncV('auth')
-  const router = useRouter()
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const activeTheme = useSyncLocalStorage("activeTheme");
+  const animate = useSyncLocalStorage("animate");
+  const { authUser } = useSyncV("auth");
+  const router = useRouter();
 
   class eventsHandler {
     static themeHandler = () => {
       setSyncLocalStorage(
-        'activeTheme',
-        activeTheme === 'dark' ? 'light' : 'dark'
-      )
-    }
+        "activeTheme",
+        activeTheme === "dark" ? "light" : "dark"
+      );
+    };
     static handleOpenNavMenu = (e) => {
-      setAnchorElNav(e.currentTarget)
-    }
+      setAnchorElNav(e.currentTarget);
+    };
     static handleOpenUserMenu = (e) => {
-      setAnchorElUser(e.currentTarget)
-    }
+      setAnchorElUser(e.currentTarget);
+    };
     static handleCloseNavMenu = () => {
-      setAnchorElNav(null)
-    }
+      setAnchorElNav(null);
+    };
     static handleCloseUserMenu = () => {
-      setAnchorElUser(null)
-    }
+      setAnchorElUser(null);
+    };
     static handleClickNavMenu = (route) => {
-      router.push(route)
-    }
+      router.push(route);
+    };
     static animateHandler = () => {
-      setSyncLocalStorage('animate', animate ? false : true)
-    }
+      setSyncLocalStorage("animate", animate ? false : true);
+    };
   }
   const {
     themeHandler,
@@ -93,34 +94,34 @@ function Header() {
     handleCloseNavMenu,
     handleCloseUserMenu,
     handleClickNavMenu,
-    animateHandler
-  } = eventsHandler
+    animateHandler,
+  } = eventsHandler;
 
   return (
     <AppBar
       elevation={10}
       sx={{
-        position: 'relative',
+        position: "relative",
         zIndex: 100,
-        background: 'inherit',
-        backdropFilter: 'blur(5px)',
+        background: "inherit",
+        backdropFilter: "blur(5px)",
       }}
       id="appBar"
     >
       <Container maxWidth="xxl">
         <Toolbar disableGutters>
-          <Link href="/" style={{ textDecoration: 'none', display: 'flex' }}>
+          <Link href="/" style={{ textDecoration: "none", display: "flex" }}>
             <Typography
               className={Avalon.className}
               variant="h6"
               noWrap
               sx={{
                 mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontFamily: 'monospace',
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
                 fontWeight: 700,
-                letterSpacing: '.1rem',
-                textDecoration: 'none',
+                letterSpacing: ".1rem",
+                textDecoration: "none",
                 color: (theme) => theme.palette.text.primary,
               }}
             >
@@ -128,7 +129,7 @@ function Header() {
             </Typography>
           </Link>
 
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -143,18 +144,18 @@ function Header() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
               {pages.map((page) => (
@@ -167,10 +168,10 @@ function Header() {
           <Link
             href="/"
             style={{
-              textDecoration: 'none',
-              display: 'flex',
+              textDecoration: "none",
+              display: "flex",
               flex: 1,
-              justifyContent: 'center',
+              justifyContent: "center",
             }}
           >
             <Typography
@@ -178,31 +179,31 @@ function Header() {
               variant="h5"
               noWrap
               sx={{
-                display: { xs: 'flex', md: 'none' },
+                display: { xs: "flex", md: "none" },
                 fontWeight: 700,
-                letterSpacing: '.1rem',
-                textDecoration: 'none',
-                marginTop: '5px',
+                letterSpacing: ".1rem",
+                textDecoration: "none",
+                marginTop: "5px",
                 color: (theme) => theme.palette.text.primary,
               }}
             >
               myPhonebook
             </Typography>
           </Link>
-          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
               <Link
                 key={page}
                 href={`/${page}`}
-                style={{ textDecoration: 'none' }}
+                style={{ textDecoration: "none" }}
               >
                 <Button
                   onClick={handleCloseNavMenu}
                   sx={{
                     my: 2,
                     color: (theme) => theme.palette.text.primary,
-                    display: 'block',
-                    fontWeight: 'bold',
+                    display: "block",
+                    fontWeight: "bold",
                   }}
                 >
                   {page}
@@ -223,23 +224,23 @@ function Header() {
                 </IconButton>
               </Tooltip>
               <Menu
-                sx={{ mt: '45px' }}
+                sx={{ mt: "45px" }}
                 id="menu-appbar"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
               >
                 {settings.map((setting, id) => {
-                  return setting.name === 'Logout' ? (
+                  return setting.name === "Logout" ? (
                     <MenuItem key={id} onClick={() => authSignOut()}>
                       <Typography textAlign="center">{setting.name}</Typography>
                     </MenuItem>
@@ -250,7 +251,7 @@ function Header() {
                     >
                       <Typography textAlign="center">{setting.name}</Typography>
                     </MenuItem>
-                  )
+                  );
                 })}
               </Menu>
             </Box>
@@ -258,21 +259,21 @@ function Header() {
             <>
               <Box
                 sx={{
-                  display: 'flex',
+                  display: "flex",
                 }}
               >
-                <Link href="/login" style={{ textDecoration: 'none' }}>
+                <Link href="/login" style={{ textDecoration: "none" }}>
                   <Button
                     sx={{
                       my: 2,
-                      color: 'black',
-                      display: { xs: 'none', md: 'flex' },
+                      color: "black",
+                      display: { xs: "none", md: "flex" },
                     }}
                   >
                     <Typography
                       variant="h8"
                       sx={{
-                        fontWeight: 'bold',
+                        fontWeight: "bold",
                         color: (theme) => theme.palette.text.primary,
                       }}
                     >
@@ -280,18 +281,18 @@ function Header() {
                     </Typography>
                   </Button>
                 </Link>
-                <Link href="/register" style={{ textDecoration: 'none' }}>
+                <Link href="/register" style={{ textDecoration: "none" }}>
                   <Button
                     sx={{
                       my: 2,
-                      color: 'black',
-                      display: { xs: 'none', md: 'flex' },
+                      color: "black",
+                      display: { xs: "none", md: "flex" },
                     }}
                   >
                     <Typography
                       variant="h8"
                       sx={{
-                        fontWeight: 'bold',
+                        fontWeight: "bold",
                         color: (theme) => theme.palette.text.primary,
                       }}
                     >
@@ -301,20 +302,20 @@ function Header() {
                 </Link>
               </Box>
 
-              <Link href="/login" style={{ textDecoration: 'none' }}>
+              <Link href="/login" style={{ textDecoration: "none" }}>
                 <IconButton
                   sx={{
-                    color: 'black',
-                    display: { xs: 'flex', md: 'none' },
-                    justifyContent: 'center',
-                    alignItems: 'center',
+                    color: "black",
+                    display: { xs: "flex", md: "none" },
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
                 >
-                  {activeTheme === 'dark' ? (
+                  {activeTheme === "dark" ? (
                     <LoginIcon
                       style={{
                         filter:
-                          'invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)',
+                          "invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)",
                       }}
                     />
                   ) : (
@@ -322,20 +323,20 @@ function Header() {
                   )}
                 </IconButton>
               </Link>
-              <Link href="/register" style={{ textDecoration: 'none' }}>
+              <Link href="/register" style={{ textDecoration: "none" }}>
                 <IconButton
                   sx={{
-                    color: 'black',
-                    display: { xs: 'flex', md: 'none' },
-                    justifyContent: 'center',
-                    alignItems: 'center',
+                    color: "black",
+                    display: { xs: "flex", md: "none" },
+                    justifyContent: "center",
+                    alignItems: "center",
                   }}
                 >
-                  {activeTheme === 'dark' ? (
+                  {activeTheme === "dark" ? (
                     <HowToRegIcon
                       style={{
                         filter:
-                          'invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)',
+                          "invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)",
                       }}
                     />
                   ) : (
@@ -347,38 +348,53 @@ function Header() {
           )}
           <IconButton
             sx={{
-              color: 'black',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              color: "black",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
             onClick={animateHandler}
           >
-            {activeTheme === 'dark' ? (
-              <PlayArrowIcon
-                style={{
-                  filter:
-                    'invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)',
-                }}
-              />
+            {activeTheme === "dark" ? (
+              <>
+                {animate && (
+                  <PlayArrowIcon
+                    style={{
+                      filter:
+                        "invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)",
+                    }}
+                  />
+                )}
+                {animate && (
+                  <PauseIcon
+                    style={{
+                      filter:
+                        "invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)",
+                    }}
+                  />
+                )}
+              </>
             ) : (
-              <PlayArrowIcon />
+              <>
+                {!animate && <PlayArrowIcon />}
+                {animate && <PauseIcon />}
+              </>
             )}
           </IconButton>
           <IconButton
             sx={{
-              color: 'black',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+              color: "black",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
             onClick={themeHandler}
           >
-            {activeTheme === 'dark' ? (
+            {activeTheme === "dark" ? (
               <LightModeIcon
                 style={{
                   filter:
-                    'invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)',
+                    "invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)",
                 }}
               />
             ) : (
@@ -388,6 +404,6 @@ function Header() {
         </Toolbar>
       </Container>
     </AppBar>
-  )
+  );
 }
-export default Header
+export default Header;
