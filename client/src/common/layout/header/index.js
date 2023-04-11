352e97d88bb3deaeebc8274949a36ce633862a2b
vikyw <vikyw89@gmail.com>
2023-04-11 18:06:31 +0800

    fix: layout on searchField

--- client/src/common/layout/header/index.js
+++ client/src/common/layout/header/index.js
@@ -1,109 +1,106 @@
-
-import { authSignOut } from '@/lib/hooks/useInitAuth'
-import { setSyncLocalStorage, useSyncLocalStorage } from '@/lib/hooks/useSync'
-import { AccountCircle } from '@mui/icons-material'
-import MenuIcon from '@mui/icons-material/Menu'
-import SearchIcon from '@mui/icons-material/Search'
-import { Paper, useMediaQuery } from '@mui/material'
-import Box from '@mui/material/Box'
-import IconButton from '@mui/material/IconButton'
-import Menu from '@mui/material/Menu'
-import MenuItem from '@mui/material/MenuItem'
-import Tooltip from '@mui/material/Tooltip'
-import Typography from '@mui/material/Typography'
-import { useRouter } from 'next/router'
-import { useState } from 'react'
-import { updateSyncV, useSyncV } from 'use-sync-v'
-import { SearchFieldComponent } from './searchField'
+import { authSignOut } from "@/lib/hooks/useInitAuth";
+import { setSyncLocalStorage, useSyncLocalStorage } from "@/lib/hooks/useSync";
+import { AccountCircle } from "@mui/icons-material";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import { Paper, useMediaQuery } from "@mui/material";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { updateSyncV, useSyncV } from "use-sync-v";
+import { SearchFieldComponent } from "./searchField";
 
 const loggedInSettings = [
   {
-    name: 'Profile',
-    route: '/profile',
+    name: "Profile",
+    route: "/profile",
   },
   {
-    name: 'Account',
-    route: '/',
+    name: "Account",
+    route: "/",
   },
   {
-    name: 'Logout',
-    route: '#',
+    name: "Logout",
+    route: "#",
   },
-]
+];
 const loggedOutSettings = [
   {
-    name: 'Register',
-    route: 'register',
+    name: "Register",
+    route: "register",
   },
   {
-    name: 'Login',
-    route: 'login',
+    name: "Login",
+    route: "login",
   },
   {
-    name: 'Recover Password',
-    route: 'recoverPassword',
+    name: "Recover Password",
+    route: "recoverPassword",
   },
-]
+];
 
 function Header() {
-  const router = useRouter()
-  const [setAnchorElNav] = useState(null)
-  const [anchorElUser, setAnchorElUser] = useState(null)
-  const activeTheme = useSyncLocalStorage('activeTheme')
-  const { authUser } = useSyncV('auth')
-  const isMobile = useMediaQuery('(max-width:900px)')
-  const [showSearch, setShowSearch] = useState(false)
+  const router = useRouter();
+  const [setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+  const activeTheme = useSyncLocalStorage("activeTheme");
+  const { authUser } = useSyncV("auth");
+  const isMobile = useMediaQuery("(max-width:900px)");
+  const [showSearch, setShowSearch] = useState(false);
 
   class eventsHandler {
     static themeHandler = () => {
       setSyncLocalStorage(
-        'activeTheme',
-        activeTheme === 'dark' ? 'light' : 'dark'
-      )
-    }
+        "activeTheme",
+        activeTheme === "dark" ? "light" : "dark"
+      );
+    };
     static handleOpenNavMenu = (e) => {
-      setAnchorElNav(e.currentTarget)
-    }
+      setAnchorElNav(e.currentTarget);
+    };
     static handleOpenUserMenu = (e) => {
-      setAnchorElUser(e.currentTarget)
-    }
+      setAnchorElUser(e.currentTarget);
+    };
     static handleCloseNavMenu = () => {
-      setAnchorElNav(null)
-    }
+      setAnchorElNav(null);
+    };
     static handleCloseUserMenu = () => {
-      setAnchorElUser(null)
-    }
+      setAnchorElUser(null);
+    };
     static handleClickNavMenu = (route) => {
-      router.push(route)
-    }
+      router.push(route);
+    };
   }
-  const {
-    handleOpenUserMenu,
-    handleCloseUserMenu,
-    handleClickNavMenu,
-  } = eventsHandler
+  const { handleOpenUserMenu, handleCloseUserMenu, handleClickNavMenu } =
+    eventsHandler;
 
   const showSearchHandler = () => {
-    setShowSearch((p) => !p)
-  }
+    setShowSearch((p) => !p);
+  };
 
   const toggleSidebar = () => {
-    updateSyncV('show.sidebar', (p) => !p)
-  }
+    updateSyncV("show.sidebar", (p) => !p);
+  };
 
   return (
     <Paper
       elevation={1}
       sx={{
-        position: 'relative',
+        position: "relative",
         zIndex: 100,
-        background: 'inherit',
-        backdropFilter: 'blur(5px)',
-        padding: '10px',
-        display: 'flex',
-        alignItems: 'center',
-        gap: '10px',
-        borderRadius:0
+        background: "inherit",
+        backdropFilter: "blur(5px)",
+        padding: "10px",
+        display: "flex",
+        alignItems: "center",
+        gap: "10px",
+        borderRadius: 0,
+        width:'100%'
       }}
     >
       {authUser && isMobile && (
@@ -113,34 +110,33 @@ function Header() {
       )}
       <Box
         sx={{
-          display: 'flex',
-          flex: '1',
-          justifyContent: 'right',
+          display: "flex",
+          flex: "1",
+          justifyContent: "right",
         }}
       >
-        {!showSearch && router.route === '/contacts' && (
-          <SearchFieldComponent />
-        )}
-        {router.route === '/contacts' && (
-          <IconButton
-            sx={{
-              color: 'black',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-            onClick={showSearchHandler}
-          >
-            {activeTheme === 'dark' && (
-              <SearchIcon
-                style={{
-                  filter:
-                    'invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)',
-                }}
-              />
-            )}
-            {activeTheme === 'light' && <SearchIcon />}
-          </IconButton>
+        {router.route === "/contacts" && (
+          <>
+            <SearchFieldComponent />
+            <IconButton
+              sx={{
+                color: "black",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {activeTheme === "dark" && (
+                <SearchIcon
+                  style={{
+                    filter:
+                      "invert(100%) sepia(0%) saturate(7440%) hue-rotate(111deg) brightness(126%) contrast(112%)",
+                  }}
+                />
+              )}
+              {activeTheme === "light" && <SearchIcon />}
+            </IconButton>
+          </>
         )}
       </Box>
       {authUser && (
@@ -151,23 +147,23 @@ function Header() {
             </IconButton>
           </Tooltip>
           <Menu
-            sx={{ mt: '45px' }}
+            sx={{ mt: "45px" }}
             id="menu-appbar"
             anchorEl={anchorElUser}
             anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
+              vertical: "top",
+              horizontal: "right",
             }}
             keepMounted
             transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
+              vertical: "top",
+              horizontal: "right",
             }}
             open={Boolean(anchorElUser)}
             onClose={handleCloseUserMenu}
           >
             {loggedInSettings.map((setting, id) => {
-              return setting.name === 'Logout' ? (
+              return setting.name === "Logout" ? (
                 <MenuItem key={id} onClick={() => authSignOut()}>
                   <Typography textAlign="center">{setting.name}</Typography>
                 </MenuItem>
@@ -178,7 +174,7 @@ function Header() {
                 >
                   <Typography textAlign="center">{setting.name}</Typography>
                 </MenuItem>
-              )
+              );
             })}
           </Menu>
         </>
@@ -191,17 +187,17 @@ function Header() {
             </IconButton>
           </Tooltip>
           <Menu
-            sx={{ mt: '45px' }}
+            sx={{ mt: "45px" }}
             id="menu-appbar"
             anchorEl={anchorElUser}
             anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
+              vertical: "top",
+              horizontal: "right",
             }}
             keepMounted
             transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
+              vertical: "top",
+              horizontal: "right",
             }}
             open={Boolean(anchorElUser)}
             onClose={handleCloseUserMenu}
@@ -214,12 +210,12 @@ function Header() {
                 >
                   <Typography textAlign="center">{setting.name}</Typography>
                 </MenuItem>
-              )
+              );
             })}
           </Menu>
         </>
       )}
     </Paper>
-  )
+  );
 }
-export default Header
+export default Header;
--- client/src/common/layout/page/index.js
+++ client/src/common/layout/page/index.js
@@ -1,108 +1,107 @@
-import Box from '@mui/material/Box'
+import Box from "@mui/material/Box";
 
-import Footer from '@/common/layout/footer'
-import Header from '@/common/layout/header'
-import Section from '@/common/layout/section'
-import { useSyncLocalStorage } from '@/lib/hooks/useSync'
-import { Sidebar } from '../sidebar'
-import { useSyncV } from 'use-sync-v'
-import { useMediaQuery } from '@mui/material'
+import Footer from "@/common/layout/footer";
+import Header from "@/common/layout/header";
+import Section from "@/common/layout/section";
+import { useSyncLocalStorage } from "@/lib/hooks/useSync";
+import { Sidebar } from "../sidebar";
+import { useSyncV } from "use-sync-v";
+import { useMediaQuery } from "@mui/material";
 
 const Background = () => {
-  const activeTheme = useSyncLocalStorage('activeTheme')
+  const activeTheme = useSyncLocalStorage("activeTheme");
   return (
     <Box
       sx={{
-        '&:after,:before': {
+        "&:after,:before": {
           content: '""',
-          display: 'block',
-          position: 'fixed',
-          zIndex: '-1',
+          display: "block",
+          position: "fixed",
+          zIndex: "-1",
           top: 0,
-          width: '100%',
-          height: '100%',
+          width: "100%",
+          height: "100%",
           animation: `animate 90s linear infinite, ${
-            activeTheme === 'dark' ? 'darkColorSwitcher' : 'lightColorSwitcher'
+            activeTheme === "dark" ? "darkColorSwitcher" : "lightColorSwitcher"
           } 42s linear infinite`,
         },
-        '&:after': {
-          left: '15vw',
+        "&:after": {
+          left: "15vw",
         },
-        '&:before': {
-          right: '15vw',
-          animationDelay: '-15s',
-          animationDirection: 'reverse',
+        "&:before": {
+          right: "15vw",
+          animationDelay: "-15s",
+          animationDirection: "reverse",
         },
-        '@keyframes lightColorSwitcher': {
-          '0%': {
-            backgroundColor: 'hsla(0,40%,80%,80%)',
+        "@keyframes lightColorSwitcher": {
+          "0%": {
+            backgroundColor: "hsla(0,40%,80%,80%)",
           },
-          '50%': {
-            backgroundColor: 'hsla(180,50%,80%,80%)',
+          "50%": {
+            backgroundColor: "hsla(180,50%,80%,80%)",
           },
-          '100%': {
-            backgroundColor: 'hsla(360,40%,80%,80%)',
-          },
-        },
-        '@keyframes darkColorSwitcher': {
-          '0%': {
-            backgroundColor: 'hsla(0,40%,80%,10%)',
-          },
-          '50%': {
-            backgroundColor: 'hsla(180,50%,80%,10%)',
-          },
-          '100%': {
-            backgroundColor: 'hsla(360,40%,80%,10%)',
+          "100%": {
+            backgroundColor: "hsla(360,40%,80%,80%)",
           },
         },
-        '@keyframes animate': {
+        "@keyframes darkColorSwitcher": {
+          "0%": {
+            backgroundColor: "hsla(0,40%,80%,10%)",
+          },
+          "50%": {
+            backgroundColor: "hsla(180,50%,80%,10%)",
+          },
+          "100%": {
+            backgroundColor: "hsla(360,40%,80%,10%)",
+          },
+        },
+        "@keyframes animate": {
           from: {
-            transform: 'rotate(0deg)',
+            transform: "rotate(0deg)",
           },
           to: {
-            transform: 'rotate(360deg)',
+            transform: "rotate(360deg)",
           },
         },
       }}
     ></Box>
-  )
-}
+  );
+};
 
 function Page({ children }) {
-  const animate = useSyncLocalStorage('animate')
-  const showSidebar = useSyncV('show.sidebar')
-  const isMobile = useMediaQuery('(max-width:900px)')
+  const animate = useSyncLocalStorage("animate");
+  const showSidebar = useSyncV("show.sidebar");
+  const isMobile = useMediaQuery("(max-width:900px)");
 
   return (
     <>
       {animate && <Background />}
       <Box
         sx={{
-          position: 'fixed',
-          top: '0',
-          left: '0',
-          bottom: '0',
-          right: '0',
-          display: 'flex',
-          overflowX: 'hidden',
+          position: "fixed",
+          top: "0",
+          left: "0",
+          bottom: "0",
+          right: "0",
+          display: "flex",
         }}
       >
         {(showSidebar || !isMobile) && <Sidebar />}
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '100vw',
+            display: "flex",
+            flexDirection: "column",
+            border: "1px solid red",
+            width:`${isMobile ? '100%' : 'calc(100vw - 200px)'}`
           }}
         >
           <Header />
-
           <Section>{children}</Section>
           <Footer />
         </Box>
       </Box>
     </>
-  )
+  );
 }
 
-export default Page
+export default Page;
